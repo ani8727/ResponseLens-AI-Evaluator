2c77e9eef2e2ca251712374062216791b4a8827a
ani8727 <aniketgupta.8727@gmail.com>
2026-05-05 23:54:01 +0530

    update

--- backend/src/main/java/com/responselens/backend/controller/AuthController.java
+++ backend/src/main/java/com/responselens/backend/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
